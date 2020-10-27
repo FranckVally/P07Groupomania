60ed470e644070734aeb61efa8628cd5184d7688
Franck VALLY <franck.vally@laposte.net>
2020-10-27 20:51:32 +0100

    nine

--- front/src/views/Wall.vue
+++ front/src/views/Wall.vue
@@ -1,57 +1,60 @@
 <template>
   <div id="wall" class="wall">
     <CreatePost />
-    <Post v-for="post in allPosts" v-bind:key="post.id" :post="post" @infosPost="setInfos" />
+    <Post
+      v-for="post in allPosts"
+      v-bind:key="post.id"
+      :post="post"
+      @infosPost="setInfos"
+    />
     <modalBoxModerate :post="post" />
   </div>
 </template>
 
 <script>
-import axios from "axios";
-import CreatePost from "../components/CreatePost";
-import Post from "../components/Post";
-import modalBoxModerate from "../components/ModifPost";
-
+import axios from 'axios';
+import CreatePost from '../components/CreatePost';
+import Post from '../components/Post';
+import modalBoxModerate from '../components/ModifPost';
 
 export default {
-  name: "Wall",
+  name: 'Wall',
   components: {
     CreatePost,
     Post,
-    modalBoxModerate
+    modalBoxModerate,
   },
   data() {
     return {
       post: {
-        id: "",
-        content: "",
-        image: ""
+        id: '',
+        content: '',
+        image: '',
       },
-      allPosts: []
-
+      allPosts: [],
     };
   },
   methods: {
     setInfos(payload) {
       this.post = payload.post;
-    }
+    },
   },
   mounted() {
     axios
-      .get("http://localhost:3000/api/post", {
+      .get('http://localhost:3000/api/post', {
         headers: {
-          Authorization: "Bearer " + localStorage.getItem("token")
-        }
+          Authorization: 'Bearer ' + localStorage.getItem('token'),
+        },
       })
-      .then(response => {
-        console.log("post", response.data);
+      .then((response) => {
+        console.log('post', response.data);
         this.allPosts = response.data;
       })
-      .catch(error => {
+      .catch((error) => {
         console.log(error); //affiche pas le message 'normalement' envoyé par le back
       }),
-      this.$store.dispatch("getUserInfos");
-  }
+      this.$store.dispatch('getUserInfos');
+  },
 };
 </script>
 
@@ -60,7 +63,6 @@ export default {
   width: 90%;
   margin: 5rem auto;
   padding-top: 10%;
-
 }
 .block-post {
   background-color: #c9c4c4;
@@ -70,6 +72,4 @@ export default {
   border-radius: 20px;
   margin-bottom: 1rem;
 }
-
-
 </style>
